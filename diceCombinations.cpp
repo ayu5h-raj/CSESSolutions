2be51d6bf3216925f291e811b675af66c1ead49c
Ayush Raj <ping2r3j@gmail.com>
2020-05-22 15:31:16 +0530

    solution for dice combination(memoize)

--- diceCombinations.cpp
+++ diceCombinations.cpp
@@ -1,9 +1,27 @@
-#include<bits/stdc++.h>
+//https://cses.fi/problemset/task/1633
 
+#include<bits/stdc++.h>
+#define MOD 1000000007
 using namespace std;
+int dp[1000005];
+int find(int sum){
+	if(sum == 0)
+		return 1;
+	if(sum < 0)
+		return 0;
+	if(dp[sum] != -1)
+		return dp[sum];
+	int ans = 0;
+
+	for(int i = 1 ; i <= 6 ; i++)
+		ans =  (ans%MOD + find(sum-i)%MOD)%MOD;
+	return dp[sum] = ans%MOD;
+}
 
 int main(){
 	int n ;
 	scanf("%d",&n);
-	
+	for(int i = 0 ; i <= n ; i++)
+		dp[i] = -1;
+	cout << find(n);
 }
